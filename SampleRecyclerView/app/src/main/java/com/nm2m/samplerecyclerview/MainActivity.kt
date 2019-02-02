67d4129419364db1948f387adea97925bb959391
dhrubajyotimondal <pingdjm@gmail.com>
2019-02-02 00:47:06 -0500

    Added RecycleView project

---
+++ SampleRecyclerView/app/src/main/java/com/nm2m/samplerecyclerview/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.nm2m.samplerecyclerview
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
